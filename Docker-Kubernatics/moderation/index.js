f30e273243498aab2b62b4b46000169ea5cf859d
Christon Cardoza <christon.antony_cardoza@rsa.com>
2022-02-17 23:46:25 +0530

    feat: Applied basics of kubernetes on mini-microservice using minikube and skaffold

---
+++ Docker-Kubernatics/moderation/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.post('/events', async(req, res) => {
+
+    const { type, data } = req.body;
+    console.log('Received event', type);
+
+    if( type === 'CommentCreated') {
+        const status  = data.content.includes('orange') ? 'rejected' : 'approved';
+
+        await axios.post ('http://event-bus-srv:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        })
+    }
+
+    res.send({});
+});
+
+
+app.listen(4003, () => {
+    console.log('Moderation Listening on 4003');
+});
